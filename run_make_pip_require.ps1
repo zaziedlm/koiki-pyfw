577a422895e219f9492dac4153d8e9de5504d638
Shuichi Kataoka <shu01k9@gmail.com>
2026-04-26 17:40:50 +0900

    Remove requirements.txt and associated PowerShell script for generating it

--- run_make_pip_require.ps1
+++
@@ -1,6 +0,0 @@
-poetry export -f requirements.txt -o requirements.txt --without-hashes
-
-if (-not (Select-String -Path requirements.txt -Pattern '^-e\s+\./libkoiki')) {
-    $content = Get-Content requirements.txt
-    @("-e ./libkoiki") + $content | Set-Content requirements.txt
-}
